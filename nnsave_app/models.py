848aaf2ea7a6ab0bf45e3b3b947e35b4156a7e4b
Legonigel <legonigel@gmail.com>
2016-04-16 08:49:03 -0400

    Add models and migration
     * Add location and category models
     * Migration for those models

--- nnsave_app/models.py
+++ nnsave_app/models.py
@@ -5,8 +5,22 @@ from django.contrib.gis.db import models
 class Category(models.Model):
     name = models.CharField(max_length=200)
 
+    def __unicode__(self):
+	return unicode("Category %s" % self.name)
+
 class Location(models.Model):
-    name = models.CharField(max_length=200)
+    name = models.TextField()
     phone = models.CharField(max_length=13)
-    website = models.CharField(max_length=200)
-    email = models.CharField(max_length=200)
+    website = models.URLField(blank = True)
+    email = models.EmailField(blank = True)
+    address = models.TextField(blank = True)
+    loc = models.PointField()
+    description = models.TextField()
+    category = models.ForeignKey('Category', on_delete = models.CASCADE)
+    visit_count = models.IntegerField(default = 0)
+
+    def __unicode__(self):
+	return unicode('Location %s (%s, visits %s)' %
+		       (self.name,
+			self.category.__unicode__(),
+			self.visit_count))
