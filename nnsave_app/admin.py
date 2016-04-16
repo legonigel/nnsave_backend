781dbcc848b59714272ee8bae76d419cfe24f0ba
Legonigel <legonigel@gmail.com>
2016-04-16 10:07:31 -0400

    Update admin for detectedlocation

--- nnsave_app/admin.py
+++ nnsave_app/admin.py
@@ -1,6 +1,11 @@
 from django.contrib.gis import admin
-from .models import Location, Category
+from .models import Location, Category, DetectedLocation
 
 # Register your models here.
 admin.site.register(Location, admin.OSMGeoAdmin)
 admin.site.register(Category)
+admin.site.register(DetectedLocation)
+
+admin.site.site_header = 'NNSave Admin'
+admin.site.site_title = 'NNSave site admin'
+admin.site.index_title = 'NNSave Administration'
