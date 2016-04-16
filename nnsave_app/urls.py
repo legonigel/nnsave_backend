8dcab3b18b603e520f12c3fd5a873a08f32d4a9a
Legonigel <legonigel@gmail.com>
2016-04-16 11:45:03 -0400

    Add id parameter to URL

--- nnsave_app/urls.py
+++ nnsave_app/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^location$',views.location, name='location'),
+    url(r'^locations/?(?P<id>[0-9]+)?$',views.location, name='location'),
 ]
