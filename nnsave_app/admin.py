6b83465d09d4baad06ecb83a5abd37147b7cf660
Legonigel <legonigel@gmail.com>
2016-04-16 08:55:25 -0400

    Add admin interface

--- nnsave_app/admin.py
+++ nnsave_app/admin.py
@@ -1,3 +1,6 @@
-from django.contrib import admin
+from django.contrib.gis import admin
+from .models import Location, Category
 
 # Register your models here.
+admin.site.register(Location, admin.OSMGeoAdmin)
+admin.site.register(Category)
