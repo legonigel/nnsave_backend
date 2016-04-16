53fe6a751f4808587880559ca5632be66f958a8d
Legonigel <legonigel@gmail.com>
2016-04-16 15:14:54 -0400

    Add python import export

--- nnsave_app/admin.py
+++ nnsave_app/admin.py
@@ -1,5 +1,7 @@
 from django.contrib.gis import admin
 from .models import Location, Category, DetectedLocation
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
 
 # Register your models here.
 admin.site.register(Location, admin.OSMGeoAdmin)
@@ -9,3 +11,7 @@ admin.site.register(DetectedLocation)
 admin.site.site_header = 'NNSave Admin'
 admin.site.site_title = 'NNSave site admin'
 admin.site.index_title = 'NNSave Administration'
+
+class LocationResource(resources.ModelResource):
+    class Meta:
+	model = Location
