858d3e7158af816409c2097d3e415ea3ae4851dc
Legonigel <legonigel@gmail.com>
2016-04-16 10:07:50 -0400

    Add urls and views

--- nnsave_app/views.py
+++ nnsave_app/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import JsonResponse, HttpResponse
 
 # Create your views here.
+
+def location(request):
+    response_data = {}
+    response_data['test'] = 'test_str'
+    return JsonResponse(response_data)
+
+def index(request):
+    return HttpResponse()
+
+
+def get_all_locations(request):
+    return None
