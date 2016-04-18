c2c780e3fd14448359c2738e13ffacfc729ef2ff
Legonigel <legonigel@gmail.com>
2016-04-17 22:49:22 -0400

    Initial commit for webapp - doesn't work

--- nnsave_app/urls.py
+++ nnsave_app/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import url
-
+from django.views.generic import TemplateView
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    #url(r'^/?$', TemplateView.as_view(template_name='nnsave_app/index.html'), name='index'),
     url(r'^locations/?(?P<id>[0-9]+)?$',views.location, name='location'),
     url(r'^categories/?(?P<id>[0-9]+)?$',views.category, name='category'),
 ]
